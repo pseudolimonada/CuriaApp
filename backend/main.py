38bedd14f1e722e929620e4ac908df789bf0d06b
Pedro Lima <pmc.lima@hotmail.com>
2024-11-17 18:50:01 +0000

    minor fix

---
+++ backend/main.py
@@ -0,0 +1,6 @@
+from backend.endpoints import app
+from backend.utils import logger, config
+
+if __name__ == '__main__':
+    app.run(host=config['APP_HOST'], debug=True, threaded=True, port=config['APP_PORT'])
+    logger.info(f'API v1.0 online: http://{config['APP_HOST']}:{config['APP_PORT']}')
