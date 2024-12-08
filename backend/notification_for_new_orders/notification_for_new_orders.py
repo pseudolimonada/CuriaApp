cc8d5ceb6e5008a269e0f89baf37ae462470e8dc
bigchungus9890 <33tiagohasousa33@gmail.com>
2024-12-08 22:17:10 +0000

    Still some minor fixes that were preformed on the same file...

--- backend/notification_for_new_orders/notification_for_new_orders.py
+++ backend/notification_for_new_orders/notification_for_new_orders.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
 router = APIRouter()
 
