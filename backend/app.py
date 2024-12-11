0748332a0e09041ecf0663d3198a51b6475c4b9b
Pedro Lima <pmc.lima@hotmail.com>
2024-12-11 23:17:03 +0000

    fix app host

--- backend/app.py
+++ backend/app.py
@@ -29,4 +29,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         print("Tables created")
-    app.run(debug=True, port=APP_PORT)
+    app.run(debug=True, host="0.0.0.0", port=APP_PORT)
