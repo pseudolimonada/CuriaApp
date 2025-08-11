c10ae646313376cff2ff9cc00bbd8bbdceb71ed2
bigchungus9890 <33tiagohasousa33@gmail.com>
2025-08-11 18:13:02 +0100

    added some more files...

--- frontend/frontendreact/eslint.config.mjs
+++ frontend/frontendreact/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,4 +14,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default eslintConfig;
+export default [
+  ...eslintConfig,
+  ...(Array.isArray(eslintConfigPrettier) ? eslintConfigPrettier : [eslintConfigPrettier]),
+];
