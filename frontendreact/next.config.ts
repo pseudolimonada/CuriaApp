03c952c04936e40436193282a201dedbf216907c
bigchungus9890 <33tiagohasousa33@gmail.com>
2025-08-12 20:47:58 +0100

    feat: update Next.js configuration and dependencies
    - Added experimental React compiler options in next.config.ts for improved performance.
    - Updated ESLint rules to enforce the use of the new React compiler.
    - Upgraded babel-plugin-react-compiler to version 19.1.0-rc.2.
    - Updated eslint-plugin-react-hooks to version 6.0.0-rc.1.

--- frontendreact/next.config.ts
+++ frontendreact/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    reactCompiler: {
+      compilationMode: "annotation",
+    },
+  },
+};
+module.exports = {
+  eslint: {
+    rules: {
+      "react-hooks/react-compiler": "error",
+    },
+  },
 };
 
 export default nextConfig;
