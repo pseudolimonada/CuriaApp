9985a5d1a2bf164b59454696b0c8a9efa5730f1a
bigchungus9890 <33tiagohasousa33@gmail.com>
2025-08-12 11:39:35 +0100

    remove commented-out prettier config from eslint configuration

--- frontendreact/eslint.config.mjs
+++ frontendreact/eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-//import { eslintConfigPrettier } from "@eslint-config-prettier/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -18,6 +17,5 @@ const eslintConfig = [
     "prettier",
   ),
 ];
-//...(Array.isArray(eslintConfigPrettier) ? eslintConfigPrettier : [eslintConfigPrettier]),
 
 export default eslintConfig;
